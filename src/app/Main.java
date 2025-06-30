73ae79b76be1daf31ca4c0bccf8c9cef96b277ee
Artem <a.fedorenko@intetics.com>
2025-06-30 19:51:24 +0100

    initial commit

---
+++ src/app/Main.java
@@ -0,0 +1,8 @@
+package app;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("App for temperature converting.");
+        System.out.println("Version 1.0.");
+    }
+}
