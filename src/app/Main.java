2e04928d5cfb6d1f43897304c8a917ad6159e204
Artem <a.fedorenko@intetics.com>
2025-06-30 21:09:58 +0100

    add convFahrenheitToCelsius

--- src/app/Main.java
+++ src/app/Main.java
@@ -1,8 +1,19 @@
 package app;
 
 public class Main {
+
+
     public static void main(String[] args) {
         System.out.println("App for temperature converting.");
         System.out.println("Version 1.0.");
+
+        double f = 10;
+        double fahrenheit = (convFahrenheitToCelsius(f));
+        System.out.println("Result is " + fahrenheit + "\" °F\"");
     }
+    private static double convFahrenheitToCelsius(double f){
+        return (f - 32) * 5 / 9;
+    }
+
+
 }
